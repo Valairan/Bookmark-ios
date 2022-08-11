200a9e2b35342658967a9284eb0a0a6857da9b87
Daryl Valairan <darylvalairan@outlook.com>
2022-08-11 02:07:41 -0400

    Initial Commit

---
+++ Bookmark/BookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkApp.swift
+//  Bookmark
+//
+//  Created by Daryl Valairan on 2022-08-11.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
